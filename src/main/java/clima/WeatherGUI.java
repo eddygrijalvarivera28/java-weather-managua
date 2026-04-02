cefa153586fa6a955c418989b3c0d1db7a3f8b81
gutierrezbrytanny08-boop <gutierrezbrytanny08@gmail.com>
2026-04-01 20:58:02 -0600

    Add WeatherGUI

---
+++ src/main/java/clima/WeatherGUI.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class WeatherGUI extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Button btn = new Button("Hello JavaFX");
+
+        Scene scene = new Scene(btn, 300, 200);
+        stage.setTitle("My First GUI");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
